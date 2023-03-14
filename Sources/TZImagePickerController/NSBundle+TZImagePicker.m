0c53f69c2008ea1b781fb326e19455c4e42f7d5b
jcc <jiangchencheng0409@gmail.com>
2023-03-14 13:49:19 +0800

    bundle

--- Sources/TZImagePickerController/NSBundle+TZImagePicker.m
+++ Sources/TZImagePickerController/NSBundle+TZImagePicker.m
@@ -8,18 +8,19 @@
 
 #import "NSBundle+TZImagePicker.h"
 #import "TZImagePickerController.h"
+#import <Foundation/Foundation.h>
 
 @implementation NSBundle (TZImagePicker)
 
 + (NSBundle *)tz_imagePickerBundle {
-//#ifdef SWIFT_PACKAGE
-////    NSBundle *bundle = SWIFTPM_MODULE_BUNDLE;
-//#else
-//
-//#endif
-    
+#ifdef SWIFT_PACKAGE
+    NSURL *bundleURL = [[[NSBundle mainBundle] bundleURL] URLByAppendingPathComponent:@"TZImagePickerController"];
+    NSBundle *bundle =[NSBundle bundleWithURL:bundleURL];
+#else
     NSBundle *bundle = [NSBundle bundleForClass:[TZImagePickerController class]];
+#endif
     
+
     NSURL *url = [bundle URLForResource:@"TZImagePickerController" withExtension:@"bundle"];
     bundle = [NSBundle bundleWithURL:url];
     return bundle;
